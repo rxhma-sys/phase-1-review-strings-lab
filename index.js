0997b3d4bb84bbfdf58e48a490df4b00f3d1f339
+ <+>
2025-03-27 08:22:57 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser= "Welcome to Flatbook, "
+ const welcomeMessage= `${currentUser}!`;
+ const excitedWelcomeMessage=`${currentUser.toUpperCase()}!`;
+ const shortGreeting= `Welcome, !`
